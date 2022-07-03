cf1ab623eb3dc6900c86b6be761ed89b6e5bba39
Chris <christian@krumms.de>
2022-07-03 16:41:28 +0200

    pre_final

--- src/main/java/APItoLink.java
+++ src/main/java/APItoLink.java
@@ -22,132 +22,130 @@ public class APItoLink {
     static JProgressBar bar = new JProgressBar();
 
     static JFrame frame = new JFrame("Ladestatus");
+    static int bilddauer;
+    static String bilddauerString;
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        JTextField urlEingabe = new JTextField(30);
+        JTextField bildanzeigedauer = new JTextField(3);
+        JPanel eingabe = new JPanel();
 
-        //JFrame frame = new JFrame("Ladestatus");
+        eingabe.add(new JLabel("Wiki URL: "));
+        eingabe.add(urlEingabe);
+        eingabe.add(Box.createHorizontalStrut(30));
+        eingabe.add(new JLabel("Geschwindigkeit Slideshow (in Sekunden): "));
+        eingabe.add((bildanzeigedauer));
 
-        url = JOptionPane.showInputDialog(frame, "Wiki URL");
+        int result = JOptionPane.showConfirmDialog(null, eingabe,
+                "Wiki Url", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            url = urlEingabe.getText();
+            bilddauerString = bildanzeigedauer.getText();
+            bilddauer = Integer.valueOf(bilddauerString)*1000/6;
 
-        bar.setValue(0);
-        bar.setBounds(0, 0, 420, 50);
-        bar.setStringPainted(true);
-        frame.add(bar);
-        frame.setSize(420, 150);
-        frame.setLocation(450, 250);
-        frame.setLayout(null);
-        URL url2 = new URL(url);
-        String[] segments = url2.getPath().split("/");
-        idStr = segments[segments.length - 1];
+            //JFrame frame = new JFrame("Ladestatus");
 
-        syncRequest();
-        frame.setVisible(true);
-        bar.setVisible(true);
+            //url = JOptionPane.showInputDialog(frame, "Wiki URL");
 
-        System.out.println(idStr);
-        links();
+            bar.setValue(0);
+            bar.setBounds(0, 0, 420, 50);
+            bar.setStringPainted(true);
+            frame.add(bar);
+            frame.setSize(420, 150);
+            frame.setLocation(450, 250);
+            frame.setLayout(null);
+            URL url2 = new URL(url);
+            String[] segments = url2.getPath().split("/");
+            idStr = segments[segments.length - 1];
 
-        long time = System.currentTimeMillis();
-        new Thread(new Runnable() {
+            syncRequest();
+            frame.setVisible(true);
+            bar.setVisible(true);
 
-            @Override
-            public void run() {
-                boolean sichtbar = true;
-                //while (true) {
-                    //if (System.currentTimeMillis() - time > 8500) {
-                    //if (counter > 100) {
-                     //   frame.setVisible(false);
-                      //  sichtbar = false;
-                      //  break;
-                  //  }
-                //}
-            }
-        }).start();
-        PApplet.main("Slideshowprocessing");
+            System.out.println(idStr);
+            links();
 
+            new Thread(new Runnable() {
 
+                @Override
+                public void run() {
+
+                }
+            }).start();
+            PApplet.main("Slideshowprocessing");
 
 
-        //fill();
 
-
+        }
     }
 
 
-    public static void links() {
+        public static void links () {
 
-        String list = response2.body().toString();
-        for (int i = 10; i < list.length(); i++) {
-            if (list.substring(i - 4, i).equals("\"url")) {
-                int j = i;
-                while (true) {
-                    if (list.substring(j - 3, j).equalsIgnoreCase("png") || list.substring(j - 3, j).equalsIgnoreCase("jpg") || list.substring(j - 4, j).equalsIgnoreCase("jpeg")) {
-                        Linkliste.add(list.substring(i + 3, j));
-                        break;
+            String list = response2.body().toString();
+            for (int i = 10; i < list.length(); i++) {
+                if (list.substring(i - 4, i).equals("\"url")) {
+                    int j = i;
+                    while (true) {
+                        if (list.substring(j - 3, j).equalsIgnoreCase("png") || list.substring(j - 3, j).equalsIgnoreCase("jpg") || list.substring(j - 4, j).equalsIgnoreCase("jpeg")) {
+                            Linkliste.add(list.substring(i + 3, j));
+                            break;
+                        }
+                        if (list.substring(j - 3, j).equalsIgnoreCase("svg") || list.substring(j - 3, j).equalsIgnoreCase("tif")) {
+                            break;
+                        }
+                        j++;
                     }
-                    if (list.substring(j - 3, j).equalsIgnoreCase("svg") || list.substring(j - 3, j).equalsIgnoreCase("tif")) {
-                        break;
-                    }
-                    j++;
                 }
             }
+            for (int p = 0; p < Linkliste.size(); p++) {
+                System.out.println(Linkliste.get(p));
+            }
+            ladezyklen = 100 / Linkliste.size();
         }
-        for (int p = 0; p < Linkliste.size(); p++) {
-            System.out.println(Linkliste.get(p));
+
+
+        public static int find () {
+            return response.body().toString().indexOf("pageid");
+
+
         }
-        ladezyklen = 100 / Linkliste.size();
-    }
 
+        public static String pageid () {
+            String id;
 
-    public static int find() {
-        return response.body().toString().indexOf("pageid");
-
-
-    }
-
-    public static String pageid() {
-        String id = "";
-
-        id = response.body().toString();
-        id = id.substring((find() + 8), response.body().toString().indexOf(",\"ns"));
-        return id;
-    }
-
-
-    private static void syncRequest() throws IOException, InterruptedException {
-
-        String name = idStr;
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(URI.create("https://de.wikipedia.org/w/api.php?action=query&format=json&titles=" + name)).build();
-        response = client.send(request, BodyHandlers.ofString());
-        var request2 = HttpRequest.newBuilder(URI.create("https://de.wikipedia.org/w/api.php?action=query&pageids=" + pageid() +
-                "&generator=images&prop=imageinfo&iiprop=url&format=json&gimlimit=15")).build();
-        response2 = client.send(request2, BodyHandlers.ofString());
-
-    }
-
-    public static int ladezyklen;
-    public static int counter = 0;
-
-    public static void setCounter() {
-        counter = counter + ladezyklen;
-        bar.setValue(counter);
-
-        if (counter >= 90) {
-            frame.setVisible(false);
-            bar.setVisible(false);
+            id = response.body().toString();
+            id = id.substring((find() + 8), response.body().toString().indexOf(",\"ns"));
+            return id;
         }
-        //else {
-        //    frame.setVisible(true);
-        //    bar.setVisible(true);
-        //}
+
+
+        private static void syncRequest () throws IOException, InterruptedException {
+
+            String name = idStr;
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(URI.create("https://de.wikipedia.org/w/api.php?action=query&format=json&titles=" + name)).build();
+            response = client.send(request, BodyHandlers.ofString());
+            var request2 = HttpRequest.newBuilder(URI.create("https://de.wikipedia.org/w/api.php?action=query&pageids=" + pageid() +
+                    "&generator=images&prop=imageinfo&iiprop=url&format=json&gimlimit=15")).build();
+            response2 = client.send(request2, BodyHandlers.ofString());
+
+        }
+
+        public static int ladezyklen;
+        public static int counter = 0;
+
+        public static void setCounter () {
+            counter = counter + ladezyklen;
+            bar.setValue(counter);
+
+            if (counter >= 90) {
+                frame.setVisible(false);
+                bar.setVisible(false);
+            }
+
+        }
+
+
     }
-
-    /*public static void fill() {
-
-        ladezyklen = 100 / Linkliste.size();
-
-    }*/
-
-}
--- src/main/java/Slideshowprocessing.java
+++ src/main/java/Slideshowprocessing.java
@@ -21,7 +21,7 @@ public class Slideshowprocessing extends PApplet {
 	
 	
 	public void settings() {
-		size(1200,1200);
+		size(1280,720);
 		
 		
 	}
@@ -35,59 +35,104 @@ public class Slideshowprocessing extends PApplet {
 	
 	
 	public void draw() {
-
-
-		if (APItoLink.counter >= 100)
-			surface.setVisible(true);
 		surface.setLocation(100,0);
 		background(255);
+
+
+
+
+
+
+		if (weiterLaden)
+		loadImages();
+
 		
+
+	 	  
+	  
+		if (!weiterLaden) {
+			image(imgsBin.get(counter), 0, 0, 1280, 720);
+			counter++;
+		}
+
+		if (!weiterLaden)
+			surface.setVisible(true);
+
+		if(counter==APItoLink.Linkliste.size())
+			counter=0;
+		/*try {
+			Thread.sleep(2500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		if (!weiterLaden)
+			System.out.println("Warte "+ Integer.valueOf(APItoLink.bilddauerString) +" Sekunden");
+		delay(APItoLink.bilddauer);
+		if (!weiterLaden)
+			System.out.print(".");
+		delay(APItoLink.bilddauer);
+		if (!weiterLaden)
+			System.out.print(".");
+		delay(APItoLink.bilddauer);
+		if (!weiterLaden)
+			System.out.print(".");
+		delay(APItoLink.bilddauer);
+		if (!weiterLaden)
+			System.out.print(".");
+		delay(APItoLink.bilddauer);
+		if (!weiterLaden)
+			System.out.print(".");
+		delay(APItoLink.bilddauer);
+		if (!weiterLaden)
+			System.out.println(".");
+		if (!weiterLaden)
+			System.out.println("Zeige nächstes Bild:");
+	 
+	}
+
+	private void loadImages() {
+
 		Thread t1 = new Thread(()->{
-			downloadfirsthalf();
+			loadingThread_1();
 		});
-		
+
 		Thread t2 = new Thread(()->{
-			downloadsecondhalf();
+			loadingThread_2();
 		});
-		
+
 
 		t1.start();
 		t2.start();
-	 	  
-	  
-		try {
-		Thread.sleep(2500);
-		} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		image(imgsBin.get(counter),0,0,1200,1200);
-		counter++;
-	
-		if(counter==APItoLink.Linkliste.size())
-			counter=0;
-	
-	 
+
+
 	}
-	
-	public void downloadfirsthalf() {
+
+	boolean weiterLaden = true;
+
+	public void loadingThread_1() {
 		for (int i =0; i<APItoLink.Linkliste.size()/2; i++) {
 			lock.lock();
 			imgsBin.add(loadImage(APItoLink.Linkliste.get(i)));
 			lock.unlock();
+			System.out.println("Thread1: Downloading Image: " +APItoLink.Linkliste.get(i));
+
 			APItoLink.setCounter();
+
 		}
+		weiterLaden = false;
 	}
 	
-	public void downloadsecondhalf() {
+	public void loadingThread_2() {
 		for (int i =APItoLink.Linkliste.size()/2; i<APItoLink.Linkliste.size(); i++) {
 			lock.lock();
 			imgsBin.add(loadImage(APItoLink.Linkliste.get(i)));	
 			lock.unlock();
+			System.out.println("Thread2: Downloading Image: " +APItoLink.Linkliste.get(i));
 			APItoLink.setCounter();
 		}
-	
-	
+		weiterLaden = false;
+
 	}
 	
 	
